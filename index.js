15f2fa4ea37d35b49132a4c1adc4620618ff17ba
Erlon Carlos de Oliveira Gomes <erlonlon@gmail.com>
2021-04-29 20:52:45 -0300

    cotação pelo banco central

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const convert = require('./lib/convert')
+const apiBCB = require('./lib/api.bcb')
 
 const app = express()
 
@@ -8,8 +9,9 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (re, res) => {
-  res.render('home')
+app.get('/', async (re, res) => {
+  const cotacao = await apiBCB.getCotacao()
+  res.render('home', { cotacao })
 })
 
 app.get('/cotacao', (req, res) => {
@@ -31,7 +33,7 @@ app.get('/cotacao', (req, res) => {
   }
 })
 
-app.listen(3001, err => {
+app.listen(3000, err => {
   if (err) {
     console.log('Erro ao ocnectar servidor')
   } else {
